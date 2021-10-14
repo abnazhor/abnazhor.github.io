dcf512a8d3c4228c96281f64a8105d6c37ee79c6
abnazhor <jorbar982@outlook.com>
2021-10-14 23:53:46 +0200

    Improvements in loading times

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -3,7 +3,7 @@
     export let selected = false;
 </script>
 
-<a class="md:text-lg font-semibold" {href} class:selected={selected}>
+<a class="md:text-lg text-green-900 font-semibold" {href} class:selected={selected}>
     <slot></slot>
 </a>
 
